c12140d5ea1a3e7bf93bbb27084a5beb432b4dfb
luuuuyang <luuuuyang@163.com>
2022-05-27 21:49:17 +0800

    Require PuerTS

---
+++ Assets/Gen/Typing/csharp/index.d.ts
@@ -0,0 +1,44 @@
+﻿
+declare module 'csharp' {
+    namespace CSharp {
+        interface $Ref<T> {
+            value: T
+        }
+        namespace System {
+            interface Array$1<T> extends System.Array {
+                get_Item(index: number):T;
+                set_Item(index: number, value: T):void;
+            }
+        }
+        interface $Task<T> {}
+        namespace System {
+            class Array extends System.Object implements System.ICloneable, System.Collections.IEnumerable, System.Collections.IList, System.Collections.IStructuralComparable, System.Collections.IStructuralEquatable, System.Collections.ICollection
+            {
+            }
+            class Object
+            {
+            }
+            interface ICloneable
+            {
+            }
+        }
+        namespace System.Collections {
+            interface IEnumerable
+            {
+            }
+            interface IList extends System.Collections.IEnumerable, System.Collections.ICollection
+            {
+            }
+            interface ICollection extends System.Collections.IEnumerable
+            {
+            }
+            interface IStructuralComparable
+            {
+            }
+            interface IStructuralEquatable
+            {
+            }
+        }
+    }
+    export = CSharp;
+}
