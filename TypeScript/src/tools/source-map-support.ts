a97a2bff8b172eb65fddaa97708ad39cafa838a7
luuuuyang <luuuuyang@163.com>
2022-05-31 17:17:16 +0800

    add ts-ignore

--- TypeScript/src/tools/source-map-support.ts
+++ TypeScript/src/tools/source-map-support.ts
@@ -1,11 +1,14 @@
 import csharp = require("csharp");
 
 (function () {
+    // @ts-ignore
     let puerts = (this ?? globalThis)["puerts"];
     puerts.registerBuildinModule("path", {
+        // @ts-ignore
         dirname(path) {
             return csharp.System.IO.Path.GetDirectoryName(path);
         },
+        // @ts-ignore
         resolve(dir, url) {
             url = url.replace(/\\/g, "/");
             while (url.startsWith("../")) {
@@ -16,14 +19,16 @@ import csharp = require("csharp");
         },
     });
     puerts.registerBuildinModule("fs", {
+        // @ts-ignore
         existsSync(path) {
             return csharp.System.IO.File.Exists(path);
         },
+        // @ts-ignore
         readFileSync(path) {
             return csharp.System.IO.File.ReadAllText(path);
         },
     });
-
+    // @ts-ignore
     let global = this ?? globalThis;
     global["Buffer"] = global["Buffer"] ?? {};
 })();
