17ee67f8c938550ae0d6fcae565552936414ab13
luuuuyang <luuuuyang@163.com>
2022-06-12 14:38:18 +0800

    Update demo

--- TypeScript/src/gen/MainMenu.ts
+++ TypeScript/src/gen/MainMenu.ts
@@ -8,49 +8,33 @@ import { Cube } from "./cube"
 
 class MainMenu implements UIBase {
 	public gameObject: GameObject
-	private btnInstantiate: GameObject
-	private btnRotate: GameObject
-	private btnPause: GameObject
-	private btnDestroy: GameObject
+	private btnStart: GameObject;
+	private btnSetting: GameObject;
+	private btnExit: GameObject;
 	constructor(gameObject: GameObject) {
 		this.gameObject = gameObject
 		let propsComponent = this.gameObject.GetComponent($typeof(TSProperties)) as TSProperties
-		this.btnInstantiate = propsComponent.Pairs.get_Item(0).value
-		this.btnRotate = propsComponent.Pairs.get_Item(1).value
-		this.btnPause = propsComponent.Pairs.get_Item(2).value
-		this.btnDestroy = propsComponent.Pairs.get_Item(3).value
+		this.btnStart = propsComponent.Pairs.get_Item(0).value
+		this.btnSetting = propsComponent.Pairs.get_Item(1).value
+		this.btnExit = propsComponent.Pairs.get_Item(2).value
 	}
 	OnStart(): void {
         let mainMenuCanvas = this.gameObject.GetComponent($typeof(UnityEngine.Canvas)) as UnityEngine.Canvas
         let mainCamera = GameObject.Find("Main Camera")
         mainMenuCanvas.worldCamera = mainCamera.GetComponent($typeof(UnityEngine.Camera)) as UnityEngine.Camera
 
-		let cube: Cube
-
-		let btnInstantiate = this.btnInstantiate.GetComponent($typeof(UnityEngine.UI.Button)) as UnityEngine.UI.Button
-		btnInstantiate.onClick.AddListener(() => {
-			console.log("click btnInstantiate")
-			let instantiateObject = ObjectManager.InstantiateAsync(Cube)
-			instantiateObject.then(object => {
-				console.log("InstantiateAsync completed 3")
-				cube = object
-			})
+		let btnStart = this.btnStart.GetComponent($typeof(UnityEngine.UI.Button)) as UnityEngine.UI.Button
+		btnStart.onClick.AddListener(() => {
+			UIManager.Close(this)
+			ObjectManager.InstantiateAsync(Cube)
 		})
 
-		let btnRotate = this.btnRotate.GetComponent($typeof(UnityEngine.UI.Button)) as UnityEngine.UI.Button
-		btnRotate.onClick.AddListener(() => {
-			console.log("click btnRotate")
+		let btnSetting = this.btnSetting.GetComponent($typeof(UnityEngine.UI.Button)) as UnityEngine.UI.Button
+		btnSetting.onClick.AddListener(() => {
 		})
 
-		let btnPause = this.btnPause.GetComponent($typeof(UnityEngine.UI.Button)) as UnityEngine.UI.Button
-		btnPause.onClick.AddListener(() => {
-			console.log("click btnPause")
-		})
-
-		let btnDestroy = this.btnDestroy.GetComponent($typeof(UnityEngine.UI.Button)) as UnityEngine.UI.Button
-		btnDestroy.onClick.AddListener(() => {
-			console.log("click btnDestroy")
-			ObjectManager.Destroy(cube)
+		let btnExit = this.btnExit.GetComponent($typeof(UnityEngine.UI.Button)) as UnityEngine.UI.Button
+		btnExit.onClick.AddListener(() => {
 		})
 	}
 	OnDestroy(): void {
--- TypeScript/src/gen/cube.ts
+++ TypeScript/src/gen/cube.ts
@@ -1,5 +1,6 @@
 import { ObjectBase } from "core/interface";
-import { UnityEngine } from "csharp";
+import { DG, UnityEngine } from "csharp";
+import { Vector3 } from "Utils/Components";
 
 export class Cube implements ObjectBase {
     constructor(gameObejct: UnityEngine.GameObject) {
@@ -7,7 +8,8 @@ export class Cube implements ObjectBase {
     }
     gameObject: UnityEngine.GameObject;
     OnStart(): void {
-        
+        this.gameObject.transform.position = new Vector3(0, 1, 0)
+        this.gameObject.transform.DORotate(new Vector3(0, 180, 0), 1).SetLoops(-1)
     }
     OnDestroy(): void {
         
